c22cc45b296f110a8374edb6a039efc6cdc0579a
artem.zaholodilo <az@century.one>
2016-02-28 13:51:01 +0200

    extension class changed

--- src/MailerBundle/Controller/SiteController.php
+++ src/MailerBundle/Controller/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace MailerBundle\Controller;
 
+use MailerBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -13,5 +14,40 @@ class SiteController extends Controller
     public function indexAction()
     {
         return $this->render('MailerBundle:Site:index.html.twig');
+    }
+
+    public function loginAction(Request $request)
+    {
+        if ($userEmail = $request->request->get('email')) {
+            $this->login($userEmail);
+        }
+        return $this->render('MailerBundle:Site:login.html.twig');
+    }
+
+    public function signupAction(Request $request)
+    {
+        if ($userEMail = $request->request->get('email')) {
+            $user = new User();
+            $user->setName($userEMail);
+
+            $em = $this->getDoctrine()->getEntityManager();
+            try {
+                $em->persist($user);
+                $em->flush();
+            } catch (\Exception $ex) {
+                return $this->render('MailerBundle:Site:signup.html.twig', [
+                    'error' => 'Something want wrong, please try again latter.'
+                ]);
+            }
+        }
+
+        return $this->render('MailerBundle:Site:signup.html.twig');
+    }
+
+    private function login($email)
+    {
+
+        return true;
+
     }
 }
